c305deb2ddc745fb12e0b255494c14ec9b09f21a
Gytis Gytis <gytislaukaitis@gmail.com>
2020-11-01 12:19:12 +0200

    'initial'

---
+++ FirstTest/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FirstTest
+{
+    public class Class1
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+        }
+    }
+}
